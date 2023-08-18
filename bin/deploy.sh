c4b2783333d20111e8d9d45d046c32e2fb75b0f2
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2023-08-18 06:31:11 +0300

    add start commands

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,3 +9,8 @@ if [[ $1 = "prod" || $1 = "dev" ]] && [[ $2 = "down" || $2 = "up" ]]; then
 else
     echo 'Need to follow format ./deploy prod|dev down|up'
 fi
+
+#1 cd bin
+#2 chmod u+x deploy.sh
+#3 ./deploy.sh dev up
+#4 ./deploy.sh dev down
